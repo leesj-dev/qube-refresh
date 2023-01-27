62cc6decbb369e4f05e9eed954246c47ad7ba43d
leesj-dev <104193215+leesj-dev@users.noreply.github.com>
2023-01-27 21:24:04 +0900

    임시 파일 삭제

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from selenium.common.exceptions import NoSuchElementException
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+USER_ID = int(os.getenv("USER_ID"))
 COMMON_PATH = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.FrameLayout/androidx.viewpager.widget.ViewPager/android.widget.FrameLayout/android.widget.LinearLayout"
 desired_capabilities = {
     "platformName": "Android",
@@ -48,7 +49,7 @@ async def on_ready():
         ## Case 1. 모든 문제가 해결된 상태일 때
         if solved is None or solved == 0:
             try:
-                driver.find_element(By.XPATH, COMMON_PATH + "/android.view.ViewGroup/android.widget.LinearLayout/android.widget.TextView[1]",)
+                driver.find_element(By.XPATH, COMMON_PATH + "/android.view.ViewGroup/android.widget.LinearLayout/android.widget.TextView[1]")
                 current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
                 print(f"문제 없음 [{current_time}]")
                 solved = 0
@@ -59,12 +60,12 @@ async def on_ready():
             except NoSuchElementException:
                 if solved is None:
                     # solved가 None이라면, 변수 할당 (뒷부분과 중복이 일어나지만 어쩔 수 없음; 케이스 분류 시 코드가 너무 복잡해짐)
-                    questions = driver.find_elements(By.XPATH, COMMON_PATH + "/android.widget.RelativeLayout[2]/android.widget.LinearLayout/android.widget.GridView/android.widget.FrameLayout",)
+                    questions = driver.find_elements(By.XPATH, COMMON_PATH + "/android.widget.RelativeLayout[2]/android.widget.LinearLayout/android.widget.GridView/android.widget.FrameLayout")
                     solved = len(questions)
 
         ## Case 2. 한 문제 이상이 해결 중일 때
         # 문제 목록 반환
-        questions = driver.find_elements(By.XPATH, COMMON_PATH + "/android.widget.RelativeLayout[2]/android.widget.LinearLayout/android.widget.GridView/android.widget.FrameLayout",)
+        questions = driver.find_elements(By.XPATH, COMMON_PATH + "/android.widget.RelativeLayout[2]/android.widget.LinearLayout/android.widget.GridView/android.widget.FrameLayout")
 
         # Case 2-1. 아예 비어있을 때
         if not questions:
@@ -107,43 +108,54 @@ async def on_ready():
                         print(f"문제 선점 성공 [{current_time}]")
                         await channel.send(f"새 문제가 왔습니다 [{current_time}]")
 
-                        # Step 2. 문제 본문 & 이미지 보내기
-                        images = driver.find_elements(By.ID, "net.megastudy.qube:id/iv_chat_image")
-                        for i in range(0, len(images)):
+                        # Step 2. 문제 이미지 보내기
+                        thumbs = driver.find_elements(By.ID, "net.megastudy.qube:id/iv_chat_image")
+                        for i in range(0, len(thumbs)):
                             img_num = str(i + 1)
                             file_name = os.path.join("images", f"{current_time}-{img_num}.png")
+                            thumbs[i].click()
+                            driver.implicitly_wait(2)
+                            image = driver.find_element(By.ID, "net.megastudy.qube:id/image")
                             with open(file_name, "wb") as screenshot:
-                                screenshot.write(images[i].screenshot_as_png)
+                                screenshot.write(image.screenshot_as_png)
                             await channel.send(file=discord.File(file_name))
+                            driver.find_element(By.ID, "net.megastudy.qube:id/btn_close").click()
+                            driver.implicitly_wait(2)
+
+                        # Step 3. 문제 본문 보내기
+                        await asyncio.sleep(1)
                         messages = [
                             item.get_attribute("text")
                             for item in driver.find_elements(By.ID, "net.megastudy.qube:id/tv_chat_text")
                         ]
                         await channel.send("\n".join(messages))
 
-                        # Step 3. 풀이 옵션 선택
+                        # Step 4. 풀이 옵션 선택
                         await channel.send("지금 풀기는 1, 나중에 풀기는 2, 포기는 3을 입력하세요.")
-                        while proceed.content in range(1, 4):  # 제대로 된 input값이 들어올 때까지 반복
-                            proceed = int(await client.wait_for("message", timeout=120.0))
+                        while True:  # 제대로 된 input값이 들어올 때까지 반복
+                            proceed = await client.wait_for("message", check=lambda m: m.author.id == USER_ID, timeout=120.0)
                             print(proceed.content)
 
-                            if proceed.content == 1:
-                                await channel.send("보내실 답을 입력해주세요.")
-                                answer = await client.wait_for("message", timeout=120.0)
-                                driver.find_element(By.ID, "net.megastudy.qube:id/et_input_text").send_keys(answer)
+                            if proceed.content == "1":
+                                await channel.send("*")  # 임시 (just in case)
+                                answer = await client.wait_for("message", check=lambda m: m.author.id == USER_ID, timeout=120.0)
+                                driver.find_element(By.ID, "net.megastudy.qube:id/et_input_text").send_keys(answer.content)
                                 driver.find_element(By.ID, "net.megastudy.qube:id/btn_input_send").click()
                                 driver.find_element(By.ID, "net.megastudy.qube:id/btn_explan_complete").click()
+                                break
 
-                            elif proceed.content == 2:
+                            elif proceed.content == "2":
                                 driver.terminate_app("net.megastudy.qube")  # 앱 재실행 (다른 문제를 계속 찾기 위함)
                                 driver.activate_app("net.megastudy.qube")
                                 await asyncio.sleep(5)  # 앱이 재시작될 동안 기다림
                                 solved = len(questions)
+                                break
 
-                            elif proceed.content == 3:
+                            elif proceed.content == "3":
                                 driver.find_element(By.ID, "net.megastudy.qube:id/btn_explan_cancel").click()
-                                await asyncio.sleep(1)
+                                driver.implicitly_wait(1)
                                 driver.find_element(By.ID, "net.megastudy.qube:id/bt_positive").click()
+                                break
 
                             else:
                                 await channel.send("다시 입력해주세요.")
