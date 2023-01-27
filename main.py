fc395c096bfe56612b84d5314b5315c143cd4a32
leesj-dev <104193215+leesj-dev@users.noreply.github.com>
2023-01-27 19:39:52 +0900

    discord.py와 연동

--- main.py
+++ main.py
@@ -1,124 +1,171 @@
 import os
+import discord
+import asyncio
+import functools
+from datetime import datetime
+from dotenv import load_dotenv
 from appium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from datetime import datetime
-from dotenv import load_dotenv
+
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
-solved = int(input("현재 답변 중인 문제 수를 입력하세요 : "))
-
+COMMON_PATH = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.FrameLayout/androidx.viewpager.widget.ViewPager/android.widget.FrameLayout/android.widget.LinearLayout"
 desired_capabilities = {
     "platformName": "Android",
     "platformVersion": "13",
     "deviceName": "Android Emulator",
 }
 
+client = discord.Client(intents=discord.Intents.all())
 driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_capabilities)
-COMMON_PATH = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.FrameLayout/androidx.viewpager.widget.ViewPager/android.widget.FrameLayout/android.widget.LinearLayout"
 
-# 로딩 중인지 확인
+# 로딩 중인지 확인 (*blocking function)
 def chk_load():
     while True:
-        # current_time = datetime.now().strftime("%H:%M:%S.%f")[:-3]
         try:
-            # print("로딩중 [" + current_time + "]")
             driver.find_element(By.ID, "net.megastudy.qube:id/fl_progress")
 
         except NoSuchElementException:
             break
 
 
-while True:
-    ## Case 1. 모든 문제가 해결된 상태일 때
-    if solved == 0:
-        try:
-            driver.find_element(By.XPATH, COMMON_PATH + "/android.view.ViewGroup/android.widget.LinearLayout/android.widget.TextView[1]")
+# blocking function을 non-blocking하게 실행
+async def run_blocking(blocking_func, *args, **kwargs):
+    func = functools.partial(blocking_func, *args, **kwargs)
+    return await client.loop.run_in_executor(None, func)
 
-        # 한 문제 이상이 새로 들어왔을 때 -> Case 2
-        except NoSuchElementException:
-            pass
 
-        else:
-            current_time = datetime.now().strftime("%H:%M:%S.%f")[:-3]
-            print("문제 없음 [" + current_time + "]")
-            driver.swipe(500, 500, 500, 1000, 100)
-            chk_load()
+@client.event
+async def on_ready():
+    channel = client.get_channel(CHANNEL_ID)
+    print(f"{client.user} has connected to Discord")
+    solved = None  # 처음 실행 시에는 None으로 할당
 
-    ## Case 2. 한 문제 이상이 해결 중일 때
-    # 문제 목록 반환
-    questions = driver.find_elements(By.XPATH, COMMON_PATH + "/android.widget.RelativeLayout[2]/android.widget.LinearLayout/android.widget.GridView/android.widget.FrameLayout")
-
-    # Case 2-1. 아예 비어있을 때
-    if not questions:
-        solved = 0
-
-    else:
-        # Case 2-2. 새로운 문제가 들어왔을 때
-        if len(questions) > solved:
-
-            # 가장 뒤에서부터 신규 문제 탐색
-            for i in range(1, len(questions) + 1):
-                # 문제가 해결되었는지 점검
-                try:
-                    questions[-i].find_element(By.XPATH, ".//android.widget.ImageView")
-
-                # 해결되지 않았다면
-                except:
-                    questions[-i].click()
-                    break
-
-            # Case 2-2-1. 다른 마스터가 답변 중일 때
+    while True:
+        ## Case 1. 모든 문제가 해결된 상태일 때
+        if solved is None or solved == 0:
             try:
-                driver.find_element(By.ID, "net.megastudy.qube:id/bt_positive").click()
-                current_time = datetime.now().strftime("%H:%M:%S.%f")[:-3]
-                print("문제 선점 실패 [" + current_time + "]")
-                chk_load()
+                driver.find_element(By.XPATH, COMMON_PATH + "/android.view.ViewGroup/android.widget.LinearLayout/android.widget.TextView[1]",)
+                current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
+                print(f"문제 없음 [{current_time}]")
+                solved = 0
+                driver.swipe(500, 500, 500, 1000, 100)
+                await run_blocking(chk_load)
 
-            # Case 2-2-2. 다른 마스터가 답변 중이지 않을 때
+            # 한 문제 이상이 새로 들어왔을 때 -> Case 2
             except NoSuchElementException:
-                # Case 2-2-2-1. 내가 이미 답변한 문제인지 확인
-                try:
-                    driver.find_element(By.ID, "net.megastudy.qube:id/ibtn_close").click()
-                    chk_load()
+                if solved is None:
+                    # solved가 None이라면, 변수 할당 (뒷부분과 중복이 일어나지만 어쩔 수 없음; 케이스 분류 시 코드가 너무 복잡해짐)
+                    questions = driver.find_elements(By.XPATH, COMMON_PATH + "/android.widget.RelativeLayout[2]/android.widget.LinearLayout/android.widget.GridView/android.widget.FrameLayout",)
+                    solved = len(questions)
 
-                # Case 2-2-2-2. 내가 답변하지 않은 문제라면
-                except NoSuchElementException:
-                    current_time = datetime.now().strftime("%H:%M:%S.%f")[:-3]
-                    print("문제 선점 성공 [" + current_time + "]")
+        ## Case 2. 한 문제 이상이 해결 중일 때
+        # 문제 목록 반환
+        questions = driver.find_elements(By.XPATH, COMMON_PATH + "/android.widget.RelativeLayout[2]/android.widget.LinearLayout/android.widget.GridView/android.widget.FrameLayout",)
 
-                    while True:
-                        proceed = input("풀기는 Y, 포기하기는 N을 입력하세요: ")
-                        if proceed == "Y":
-                            # 앱 재실행 (다른 문제를 계속 찾기 위함)
-                            driver.terminate_app("net.megastudy.qube")
-                            driver.activate_app("net.megastudy.qube")
-                            input("풀이가 완료되면 아무 키나 누르세요: ")
-                            solved = len(questions)
-                            break
-
-                        elif proceed == "N":
-                            break  # 포기하기 버튼 누르기 추가
-
-                        else:
-                            print("다시 입력해주세요.")
-
-        # Case 2-3. 그대로라면
-        elif len(questions) == solved:
-            current_time = datetime.now().strftime("%H:%M:%S.%f")[:-3]
-            print("문제 없음 [" + current_time + "]")
-
-        # Case 2-4. 해결된 문제가 늘어났을 때
-        else:
-            solved = len(questions)
-
-        # 새로고침 시도
-        try:
-            driver.find_element(By.ID, "net.megastudy.qube:id/home_main_top_refresh").click()
-            chk_load()
-
-        # 모든 문제가 해결된 경우
-        except NoSuchElementException:
+        # Case 2-1. 아예 비어있을 때
+        if not questions:
             solved = 0
+
+        else:
+            # Case 2-2. 새로운 문제가 들어왔을 때
+            if len(questions) > solved:
+
+                # 가장 뒤에서부터 신규 문제 탐색
+                for i in range(1, len(questions) + 1):
+                    # 문제가 해결되었는지 점검
+                    try:
+                        questions[-i].find_element(By.XPATH, ".//android.widget.ImageView")
+
+                    # 해결되지 않았다면
+                    except NoSuchElementException:
+                        questions[-i].click()
+                        break
+
+                # Case 2-2-1. 다른 마스터가 답변 중일 때
+                try:
+                    driver.find_element(By.ID, "net.megastudy.qube:id/bt_positive").click()
+                    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
+                    print(f"문제 선점 실패 [{current_time}]")
+                    await asyncio.sleep(3)
+                    await run_blocking(chk_load)
+
+                # Case 2-2-2. 다른 마스터가 답변 중이지 않을 때
+                except NoSuchElementException:
+                    # Case 2-2-2-1. 내가 이미 답변한 문제인지 확인
+                    try:
+                        driver.find_element(By.ID, "net.megastudy.qube:id/ibtn_close").click()
+                        await run_blocking(chk_load)
+
+                    # Case 2-2-2-2. 내가 답변하지 않은 문제라면
+                    except NoSuchElementException:
+                        # Step 1. 선점 성공 알림 보내기
+                        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
+                        print(f"문제 선점 성공 [{current_time}]")
+                        await channel.send(f"새 문제가 왔습니다 [{current_time}]")
+
+                        # Step 2. 문제 본문 & 이미지 보내기
+                        images = driver.find_elements(By.ID, "net.megastudy.qube:id/iv_chat_image")
+                        for i in range(0, len(images)):
+                            img_num = str(i + 1)
+                            file_name = os.path.join("images", f"{current_time}-{img_num}.png")
+                            with open(file_name, "wb") as screenshot:
+                                screenshot.write(images[i].screenshot_as_png)
+                            await channel.send(file=discord.File(file_name))
+                        messages = [
+                            item.get_attribute("text")
+                            for item in driver.find_elements(By.ID, "net.megastudy.qube:id/tv_chat_text")
+                        ]
+                        await channel.send("\n".join(messages))
+
+                        # Step 3. 풀이 옵션 선택
+                        await channel.send("지금 풀기는 1, 나중에 풀기는 2, 포기는 3을 입력하세요.")
+                        while proceed.content in range(1, 4):  # 제대로 된 input값이 들어올 때까지 반복
+                            proceed = int(await client.wait_for("message", timeout=120.0))
+                            print(proceed.content)
+
+                            if proceed.content == 1:
+                                await channel.send("보내실 답을 입력해주세요.")
+                                answer = await client.wait_for("message", timeout=120.0)
+                                driver.find_element(By.ID, "net.megastudy.qube:id/et_input_text").send_keys(answer)
+                                driver.find_element(By.ID, "net.megastudy.qube:id/btn_input_send").click()
+                                driver.find_element(By.ID, "net.megastudy.qube:id/btn_explan_complete").click()
+
+                            elif proceed.content == 2:
+                                driver.terminate_app("net.megastudy.qube")  # 앱 재실행 (다른 문제를 계속 찾기 위함)
+                                driver.activate_app("net.megastudy.qube")
+                                await asyncio.sleep(5)  # 앱이 재시작될 동안 기다림
+                                solved = len(questions)
+
+                            elif proceed.content == 3:
+                                driver.find_element(By.ID, "net.megastudy.qube:id/btn_explan_cancel").click()
+                                await asyncio.sleep(1)
+                                driver.find_element(By.ID, "net.megastudy.qube:id/bt_positive").click()
+
+                            else:
+                                await channel.send("다시 입력해주세요.")
+
+            # Case 2-3. 그대로라면
+            elif len(questions) == solved:
+                current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
+                print(f"문제 없음 [{current_time}]")
+
+            # Case 2-4. 해결 중인 문제가 줄어들었을 때
+            else:
+                solved = len(questions)
+
+            # 새로고침 시도
+            try:
+                driver.find_element(By.ID, "net.megastudy.qube:id/home_main_top_refresh").click()
+                await run_blocking(chk_load)
+
+            # 모든 문제가 해결된 경우, 새로고침 시도가 되지 않음
+            except NoSuchElementException:
+                solved = 0
+
+
+if __name__ == "__main__":
+    client.run(TOKEN)
